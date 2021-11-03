24d848c65c240ed83e97857ba1fa32a794c97e4c
mihir.3030 <mihirdholakia777@gmail.com>
2021-11-03 17:11:34 +0530

    flask files update

--- app.py
+++ app.py
@@ -1,109 +1,59 @@
-import numpy as np
-import tensorflow as tf
 from flask import Flask, render_template, request
 import os
-import numpy as np
-import os
-import six.moves.urllib as urllib
-import sys
-import tarfile
-import tensorflow as tf
-import zipfile
-import cv2
-
-from distutils.version import StrictVersion
-from collections import defaultdict
-from io import StringIO
-from matplotlib import pyplot as plt
 from PIL import Image
+import cv2
+import numpy as np
+from load_img import load_image_into_numpy_array
+from run_inference import run_inference_for_single_image
+from graph_load import detection_graph, category_index
+from matplotlib import pyplot as plt
 
-from object_detection.utils import label_map_util
-
+IMAGE_SIZE = (12, 8)
 from object_detection.utils import visualization_utils as vis_util
-# This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("..")
-from object_detection.utils import ops as utils_ops
 
-if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
-  raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
 
+
+UPLOAD_FOLDER = './uploads'
+PEOPLE_FOLDER = os.path.join('static', 'saveimg')
 
 app = Flask(__name__)
-# What model to download.
-#MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-MODEL_NAME = 'inference_graph'
-#MODEL_FILE = MODEL_NAME + '.tar.gz'
-#DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER2'] = PEOPLE_FOLDER
 
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('training', 'labelmap.pbtxt')
-
-
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
-    
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
-
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-    
-    
-    
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
-    
-    
-    
-@app.route('/',  methods=["POST"])
-def predict():
- 
-    cap = cv2.VideoCapture(0)
-    with detection_graph.as_default():
-      with tf.Session(graph=detection_graph) as sess:
-        while True:
-          ret, image_np = cap.read()
-          # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-          image_np_expanded = np.expand_dims(image_np, axis=0)
-          image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-          # Each box represents a part of the image where a particular object was detected.
-          boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-          # Each score represent how level of confidence for each of the objects.
-          # Score is shown on the result image, together with the class label.
-          scores = detection_graph.get_tensor_by_name('detection_scores:0')
-          classes = detection_graph.get_tensor_by_name('detection_classes:0')
-          num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-          # Actual detection.
-          (boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections],feed_dict={image_tensor: image_np_expanded})
-          # Visualization of the results of a detection.
-          vis_util.visualize_boxes_and_labels_on_image_array(
-              image_np,
-              np.squeeze(boxes),
-              np.squeeze(classes).astype(np.int32),
-              np.squeeze(scores),
-              category_index,
-              use_normalized_coordinates=True,
-              line_thickness=8)
-     
-          cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
-          if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-     
-        cap.release()
-                        
-    return render_template('home.html')
+    return render_template("home.html")
 
-        
-if __name__ == '__main__':
+
+@app.route("/function", methods=["POST", 'GET'])
+def upload():
+    if request.method == "POST":
+        file = request.files['files']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(path)
+        img = Image.open(file)
+        image_np = load_image_into_numpy_array(img)
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        output_dict = run_inference_for_single_image(image_np, detection_graph)
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            output_dict['detection_boxes'],
+            output_dict['detection_classes'],
+            output_dict['detection_scores'],
+            category_index,
+            instance_masks=output_dict.get('detection_masks'),
+            use_normalized_coordinates=True,
+            line_thickness=8)
+        plt.figure(figsize=IMAGE_SIZE)
+        plt.imshow(image_np)
+        plt.imsave('./static/saveimg/image1.jpg', image_np)
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER2'], 'image1.jpg')
+        # return render_template("index.html", user_image=full_filename)
+        return render_template("home2.html", user_image=full_filename)
+    # return render_template("home3.html")
+
+
+
+if __name__ == "__main__":
     app.run(debug=True)
